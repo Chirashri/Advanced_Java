97c97dc5e89fc0cb8aab7ae7fd11ccc9afe68b00
Chirashri <154724641+Chirashri@users.noreply.github.com>
2025-05-21 19:33:28 +0530

    StringOperationDemo2a1.java

--- 2a.StringOperationDemo/StringOperationDemo2a1.java
+++ 2a.StringOperationDemo/StringOperationDemo2a1.java
@@ -1,8 +1,14 @@
+/*a. Develop a java program for performing various string operations with different string
+handling functions directed as follows
+String Creation and Basic Operations, Length and Character Access, String Comparison, String
+Searching, Substring Operations , String Modification, Whitespace Handling, String
+Concatenation, String Splitting, StringBuilder Demo, String Formatting , Validate Email with
+contains( ) and startsWith() and endsWith()*/
 package github1;
 
 import java.util.Scanner;
 
-public class pgrm2a1 {
+public class StringOperationDemo2a1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
