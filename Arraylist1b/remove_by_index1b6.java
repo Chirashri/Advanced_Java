b63e8ae2ff8ba04e0ef69dbc094ea266bcbf8f7b
Chirashri <154724641+Chirashri@users.noreply.github.com>
2025-05-21 18:52:22 +0530

    remove_by_index1b6.java

--- Arraylist1b/remove_by_index1b6.java
+++ Arraylist1b/remove_by_index1b6.java
@@ -1,7 +1,9 @@
+/*Write a java program for getting different colors through ArrayList interface and delete nth
+element from the ArrayList object by using remove by index*/
 package github1;
 import java.util.ArrayList;
 import java.util.Scanner;
-public class pgrm1b6 {
+public class remove_by_index1b6 {
 
 
 	
